4eb0ee32977c77bbebadbb9fa10f00b2bd0b173a
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-06 10:55:42 +0800

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { TFile, TFolder, Vault, App } from "obsidian";
+
+export async function calculateVaultStats(app: App) {
+	const vault = app.vault;
+	let totalFiles = 0;
+	let totalFolders = 0;
+	let totalWords = 0;
+	let totalChars = 0;
+
+	const processFolder = async (folder: TFolder) => {
+		totalFolders++;
+		for (const child of folder.children) {
+			if (child instanceof TFolder) {
+				await processFolder(child);
+			} else if (child instanceof TFile && child.extension === "md") {
+				totalFiles++;
+				const content = await vault.read(child);
+				totalChars += content.length;
+				totalWords += content.split(/\s+/).filter(word => word.length > 0).length;
+			}
+		}
+	};
+
+	await processFolder(vault.getRoot());
+
+	return {
+		totalFiles,
+		totalFolders,
+		totalWords,
+		totalChars,
+		averageWordsPerFile: totalFiles ? totalWords / totalFiles : 0
+	};
+}

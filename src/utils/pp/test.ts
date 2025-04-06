800e847a526b60e384eab29c3b33d873ecc0a85e
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-06 18:16:01 +0800

    Update test pp system

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -1,12 +1,12 @@
 /*
 
   test.ts: the test version of the osu! pp system integrated into obsidian.
-  it may vary depending on the demands.
+  this is where the pp system is being reworked before releasing as a separate pp system version, but it still may vary depending on the demands.
 
 */
 
 import { App } from "obsidian";
-import { calculateVaultStats } from "../../main";
+import { calculateVaultStats } from "../../functions/vaultStats";
 
 // the function to calculate the pp values from a single note
 export async function calculatePerformance(app: App): Promise<number> {
@@ -14,9 +14,10 @@ export async function calculatePerformance(app: App): Promise<number> {
 
   // weights
 	let a: number = 1.3228756555;
-	const b: number = 1.2247448713;
-	const c: number = 1.0488088481;
-	const d: number = 1.4142135623;
+	let b: number = 1.2247448713;
+	let c: number = 1.0488088481;
+	let d: number = 1.4142135623;
+	let e: number = 1.5811388300;
 	
   // note complexity values for the system
   const fileValue: number = vaultStats.totalFiles * (1 + (vaultStats.totalFolders / 25));
@@ -25,10 +26,21 @@ export async function calculatePerformance(app: App): Promise<number> {
   const sentenceDensityValue: number = vaultStats.totalSentences / vaultStats.totalWords;
   const fileComplexityValue: number = sentenceComplexityValue * vaultStats.averageWordsPerFile;
   const wordComplexityValue: number = vaultStats.totalChars / vaultStats.totalWords;
+
+  // bonuses and nerfs
+  const sentenceBonus: number = 0;
+
+  if (vaultStats.averageSentencesPerFile > vaultStats.averageSentencesPerParagraph) {
+    sentenceBonus = vaultStats.averageSentencesPerFile / vaultStats.averageSentencesPerParagraph;
+  } else if (vaultStats.averageSentencesPerFile < vaultStats.averageSentencesPerParagraph) {
+	sentenceBonus = vaultStats.averageSentencesPerParagraph / vaultStats.averageSentencesPerFile;
+  }
+
+  // const readabilityValue: number = 100 - ((vaultStats.averageWordsPerSentence * () / 10);
 	
-  const overallComplexityValue: number = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue;
+  const overallComplexityValue: number = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue + e * sentenceBonus;
 
   const performanceValue: number = fileValue + overallComplexityValue;
 
-  return performanceValue ;
+  return performanceValue;
 } 

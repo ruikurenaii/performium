7d30761163777e255fe25d182c06dcaaa8027d3a
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-07 07:44:52 +0800

    Divide the informativeness value (broken)

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -52,7 +52,7 @@ export async function calculatePerformance(app: App): Promise<number> {
 
   const informativenessValue: number = vaultStats.totalWords * ((vaultStats.totalChars / vaultStats.totalWords) ** 2);
 	  
-  const performanceValue: number = fileValue + overallComplexityValue + totalLengthBonus + coherenceBonus + informativenessValue;
+  const performanceValue: number = fileValue + overallComplexityValue + totalLengthBonus + coherenceBonus + (informativenessValue / sentenceComplexityValue);
 
   return performanceValue;
 } 

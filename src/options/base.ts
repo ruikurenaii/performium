75853ac12fbe02a7c254c4df2513a35d31ecbbd0
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-06 11:56:18 +0800

    Create base.ts

---
+++ src/options/base.ts
@@ -0,0 +1,44 @@
+/*
+
+  base.ts: the first tab of the options section
+
+*/
+
+import { App, PluginSettingTab, Setting } from "obsidian";
+
+export interface PerformiumBaseSettings {
+  ppSystem: string;
+}
+
+export const DEFAULT_SETTINGS: PerformiumBaseSettings = {
+  ppSystem: "040625";
+};
+
+export class CommoditySettingsTab extends PluginSettingTab {
+  plugin: any;
+
+  constructor(app: App, plugin: any) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+  const { containerEl } = this;
+  containerEl.empty();
+
+  new Setting(containerEl)
+    .setName("PP System Version")
+    .setDesc("Select a PP system used for calculaton. Remember: Different PP system means different values!")
+    .addDropdown(dropdown => {
+      dropdown.addOptions({
+        "040625": "04-06-25"
+      });
+
+      dropdown.setValue(this.plugin.settings.ppSystem);
+      dropdown.onChange(async (value) => {
+        this.plugin.settings.currency = value;
+        await this.plugin.saveSettings();
+      });
+    });
+  }
+}

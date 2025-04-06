83e56bf8dbf0e59493fea599c5d079e420162e4b
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-06 11:32:45 +0800

    Update 040625.ts

--- src/utils/pp/040625.ts
+++ src/utils/pp/040625.ts
@@ -14,15 +14,21 @@ export async function calculatePerformance(app: App): Promise<string> {
 
   // weights
 	let a: number = 1;
-	const b: number = 1.5;
-	const c: number = 3.1415926535;
+	const b: number = 1.2247448713;
+	const c: number = 1.0488088481;
+	const d: number = 1.4142135623;
 	
   // note complexity values for the system
   const fileValue: number = vaultStats.totalFiles * (1 + (vaultStats.totalFolders / 25));
+	
   const sentenceComplexityValue: number = vaultStats.totalWords / vaultStats.totalSentences;
   const sentenceDensityValue: number = vaultStats.totalSentences / vaultStats.totalWords;
-  const fileComplexityValue: number = sentenceComplexityValue * averageWordsPerFile;
-  const complexityValue;
+  const fileComplexityValue: number = sentenceComplexityValue * vaultStats.averageWordsPerFile;
+  const wordComplexityValue: number = vaultStats.totalChars / vaultStats.totalWords;
+	
+  const overallComplexityValue: number = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue;
 
-  // const performanceValue: number;
+  const performanceValue: number = fileValue + overallComplexityValue;
+
+  return performanceValue;
 }

32c0ebe57a92f0fa928e6bf36ef82911c0a45ee0
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-06 21:13:08 +0800

    fix again

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,11 @@ export default class PerformiumPlugin extends Plugin {
   } 
 
   async loadSettings() {
-  this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
   }
 }
 

b1c7bd126408ee74b9b35658fc2a2bccfc373135
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-07 15:23:11 +0800

    nerf pp values (test system)

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -13,23 +13,26 @@ export async function calculatePerformance(app: App): Promise<number> {
   const vaultStats = await calculateVaultStats(app);
 
   // weights
-	let a: number = 1.7349285739;
-	let b: number = 1.6893741205;
-	let c: number = 1.6228493017;
-	let d: number = 1.3428593012;
-	let e: number = 1.8753012946;
-	let f: number = 1.2109483725;
+  let a = 1.7349285739;
+  let b = 1.6893741205;
+  let c = 1.6228493017;
+  let d = 1.3428593012;
+  let e = 1.8753012946;
+  let f = 1.2109483725;
+
+  // factors
+  let penaltyFactor = 9.129727134;
 	
   // note complexity values for the system
-  const fileValue: number = vaultStats.totalFiles * (1 + (vaultStats.totalFolders / 25));
+  const fileValue = vaultStats.totalFiles * (1 + (vaultStats.totalFolders / 25));
 	
-  const sentenceComplexityValue: number = vaultStats.totalWords / vaultStats.totalSentences;
-  const sentenceDensityValue: number = vaultStats.totalSentences / vaultStats.totalWords;
-  const fileComplexityValue: number = sentenceComplexityValue * vaultStats.averageWordsPerFile;
-  const wordComplexityValue: number = vaultStats.totalChars / vaultStats.totalWords;
+  const sentenceComplexityValue = vaultStats.totalWords / vaultStats.totalSentences;
+  const sentenceDensityValue = vaultStats.totalSentences / vaultStats.totalWords;
+  const fileComplexityValue = sentenceComplexityValue * vaultStats.averageWordsPerFile;
+  const wordComplexityValue = vaultStats.totalChars / vaultStats.totalWords;
 
   // bonuses and nerfs
-  let sentenceBonus: number = 0;
+  let sentenceBonus = 0;
 
   if (vaultStats.averageSentencesPerFile > vaultStats.averageSentencesPerParagraph) {
     sentenceBonus = vaultStats.averageSentencesPerFile / vaultStats.averageSentencesPerParagraph;
@@ -37,27 +40,36 @@ export async function calculatePerformance(app: App): Promise<number> {
 	sentenceBonus = vaultStats.averageSentencesPerParagraph / vaultStats.averageSentencesPerFile;
   }
 
-  const readabilityMultiplier: number = 100 - ((vaultStats.averageWordsPerSentence * (vaultStats.averageCharsPerSentence / vaultStats.averageWordsPerSentence)) / 10);
-  const readabilityBonus: number = (sentenceDensityValue / sentenceDensityValue) * (readabilityMultiplier / 10);
+  const readabilityMultiplier = 100 - ((vaultStats.averageWordsPerSentence * (vaultStats.averageCharsPerSentence / vaultStats.averageWordsPerSentence)) / 10);
+  const readabilityBonus = (sentenceDensityValue / sentenceDensityValue) * (readabilityMultiplier / 10);
 	
-  const overallComplexityValue: number = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue + e * sentenceBonus + f * readabilityBonus;
+  const overallComplexityValue = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue + e * sentenceBonus + f * readabilityBonus;
 
-  const lengthBonus: number = vaultStats.longestParagraphLength / vaultStats.longestSentenceLength;
-  const sLengthBonus: number = vaultStats.longestSentenceLength / vaultStats.averageSentenceLength;
-  const paragraphBonus: number = vaultStats.longestParagraphLength / vaultStats.averageParagraphLength;
+  const lengthBonus = vaultStats.longestParagraphLength / vaultStats.longestSentenceLength;
+  const sLengthBonus = vaultStats.longestSentenceLength / vaultStats.averageSentenceLength;
+  const paragraphBonus = vaultStats.longestParagraphLength / vaultStats.averageParagraphLength;
 
-  const totalLengthBonus: number = (sLengthBonus + paragraphBonus) / lengthBonus;
+  const totalLengthBonus = (sLengthBonus + paragraphBonus) / lengthBonus;
 
-  const coherenceBonus: number = vaultStats.averageWordsPerSentence > 0 ? vaultStats.averageSentencesPerParagraph / vaultStats.averageWordsPerSentence : 0;
+  const coherenceBonus = vaultStats.averageWordsPerSentence > 0 ? vaultStats.averageSentencesPerParagraph / vaultStats.averageWordsPerSentence : 0;
 
-  const informativenessValue: number = vaultStats.totalWords * (vaultStats.totalChars / vaultStats.totalWords);
+  const informativenessValue = vaultStats.totalWords * (vaultStats.totalChars / vaultStats.totalWords);
 
   const readingLevel = (0.39 * vaultStats.averageWordsPerSentence + 11.8 * (vaultStats.averageCharsPerSentence / vaultStats.averageWordsPerSentence) - 15.59) / 5;
 
   const readingBonus = readabilityBonus * readingLevel * readabilityMultiplier;
 
+  const alternativeReadabilityValue = 0.39 * vaultStats.averageWordsPerSentence + 11.8 * wordComplexityValue;
+  const shortWordsNerf;
+
+  if (alternativeReadabilityValue > 25) {
+	shortWordsNerf = alternativeReadabilityValue * (wordComplexityValue / 5);
+  } else if (alternativeReadabilityValue < 25) {
+	shortWordsNerf = alternativeReadabilityValue - penaltyFactor * (wordComplexityValue / 5);
+  }
+														
   // gotta prevent inflation :)))))
-  const performanceValue: number = (fileValue + overallComplexityValue + totalLengthBonus + coherenceBonus + (informativenessValue ** 0.3825) + (readingBonus ** 0.5)) / 1.7724538509;
+  const performanceValue: number = (fileValue + overallComplexityValue + totalLengthBonus + coherenceBonus + (informativenessValue ** 0.3825) + (readingBonus ** 0.5) + shortWordsNerf) / 1.7724538509;
 
   return performanceValue;
 } 

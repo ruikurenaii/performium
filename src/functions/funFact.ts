3585e550abcad688b31451d8c0065a69d99c0933
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-09 06:29:51 +0800

    Update funFact.ts

--- src/functions/funFact.ts
+++ src/functions/funFact.ts
@@ -1,5 +1,16 @@
 import { formatTime } from "../utils/values/formatTime";
+import { getTimeSinceCreation } from "../../timeSinceCreation";
 
-export function generateFact() {
-  // it'll be empty for a while some functions are being made at the moment.
+// it may not be used or i don't know.
+export function generateFact(): string {
+  const array = [
+    `It has been ${getTimeSinceCreation()} since the plugin has been installed inside your vault.`,
+    `Performium is not meant to boost your application\'s performance!`,
+		`It took the developer a few days to make this plugin.`,
+    `Performium was first released on April 6, 2025.`,
+    `Some updates may take a while and this is mainly because of the performance points system reworks.`,
+    `The developer stayed up just to develop this, which is why you need to buy him a coffee to keep them awake.`
+  ];
+
+	return `Fun fact: ${array[Math.floor(Math.random() * array.length)]}`;
 }

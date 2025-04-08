9e4c115d43c8b14736c60071c568a8ab82f6f986
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-09 07:23:39 +0800

    Update funFact.ts

--- src/functions/funFact.ts
+++ src/functions/funFact.ts
@@ -6,11 +6,15 @@ export function generateFact(): string {
   const array = [
     `It has been ${getTimeSinceCreation()} since the plugin has been installed inside your vault.`,
     `Performium is not meant to boost your application\'s performance!`,
-		`It took the developer a few days to make this plugin.`,
     `Performium was first released on April 6, 2025.`,
     `Some updates may take a while and this is mainly because of the performance points system reworks.`,
-    `The developer stayed up just to develop this, which is why you need to buy him a coffee to keep them awake.`
+    `The developer stayed up just to develop this, which is why you need to buy him a coffee to keep them awake.`,
+		`Once you wrote something short and not concise, the value may get low.`,
+    `I am a fun fact that may be useless, but it may change over time.`,
+    `The higher the size of your vault, the higher you gain performance points!`,
+    `Is it just me, or something feels odd around here?`
   ];
 
-	return `Fun fact: ${array[Math.floor(Math.random() * array.length)]}`;
+	let fact: string = `Fun fact: ${array[Math.floor(Math.random() * array.length)]}`;
+	return fact;
 }

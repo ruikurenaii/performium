3d3d412f90d3d4a782b911b637dd209307ac590d
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-09 08:33:37 +0800

    Update and rename timeSinceCreation.ts to timeSinceDate.ts

--- src/functions/timeSinceDate.ts
+++ src/functions/timeSinceDate.ts
@@ -1,7 +1,6 @@
 import { timeFormat } from "../utils/values/timeFormat";
 
-export function getTimeSinceCreation(): string {
-  const installDate = this.settings.installTimestamp ?? Date.now();
+export function getTimeSinceDate(installDate: number): string {
   const timeElapsed = Date.now() - installDate;
 
   return timeFormat(timeElapsed);

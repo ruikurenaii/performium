736a84f434d42b1ae8e195285d96b5737249c960
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-04-09 09:28:32 +0800

    well, I guess that did not render correctly

--- src/functions/funFact.ts
+++ src/functions/funFact.ts
@@ -4,18 +4,19 @@ import { getTimeSinceDate } from "./timeSinceDate";
 // it may not be used or i don't know.
 export function generateFact(installTimestamp: number): string {
   const array = [
-    `It has been ${getTimeSinceDate(installTimestamp)} since the plugin has been installed inside your vault.`,
+    `It has been ${getTimeSinceDate(installTimestamp)} since the plugin has been installed inside your vault!`,
     `Performium is not meant to boost your application\'s performance!`,
-    `Performium was first released on April 6, 2025.`,
-    `Some updates may take a while and this is mainly because of the performance points system reworks.`,
-    `The developer himself didn't get to sleep because of staying all night developing this plugin without a coffe, which is why you need to buy one <a href="https://ko-fi.com/paytouse1774">here</a>!`,
-		`Once you wrote something short and not concise, the value may get low.`,
-    `I am a fun fact that may be useless, but it may change every time you open this.`,
+    `Performium was first released on April 6, 2025!`,
+    `Some updates may take a while and this is mainly because of the performance points system reworks!`,
+    `The developer himself didn't get to sleep because of staying all night developing this plugin without a coffee, which is why you need to buy one located in the plugin's page!`,
+		`Once you wrote something short and not concise, the value may get low!`,
+    `This is a fun fact that may be useless, but it may change every time you open this?`,
     `The higher the size of your vault, the higher you gain performance points!`,
     `Is it just me, or something feels odd around here?`,
     `Older performance point systems can be put in the settings for future updates!`,
     `Chicken jockey!!`,
-    `The developer is an osu! player that goes under the name of hoshinoTaka01?`
+    `The developer is an osu! player that goes under the name of hoshinoTaka01?`,
+    `The creation date of Performium was also the same day as it was released!`
   ];
 
 	let fact: string = array[Math.floor(Math.random() * array.length)];

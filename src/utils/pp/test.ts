9fc155ce74111d07953ff3020885b802b9cfed0e
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-17 23:44:22 +0800

    deflate timeBonus value

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -37,7 +37,7 @@ export async function calculatePerformance(app: App): Promise<number> {
   const lengthBonus = vaultStats.longestParagraphLength / vaultStats.longestSentenceLength;
   const timeBonus = Math.sqrt(Math.sqrt(totalFocusTime / (Math.sqrt(totalFocusTime) / totalFocusTime)));
 
-  const performanceValue: number = files + wordsPerFile + tags + lengthBonus + (1 + (starRating * 0.1)) + timeBonus;
+  const performanceValue: number = files + wordsPerFile + tags + lengthBonus + (1 + (starRating * 0.1)) + (timeBonus / 9.8);
 
   return performanceValue;
 }

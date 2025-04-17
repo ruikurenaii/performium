c2c26aadbdce531002380720b26e4b42d113c749
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-17 18:43:53 +0800

    Update getUptime.ts

--- src/functions/getUptime.ts
+++ src/functions/getUptime.ts
@@ -1,12 +1,9 @@
-export function getUptime(startTime: number): {
+export function getUptime(uptime: number): {
   days: number;
   hours: number;
   minutes: number;
   seconds: number;
 } {
-  const now = Date.now();
-  const uptime = now - startTime;
-
   const days = Math.floor(uptime / (1000 * 60 * 60 * 24));
   const hours = Math.floor((uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((uptime % (1000 * 60 * 60)) / (1000 * 60));

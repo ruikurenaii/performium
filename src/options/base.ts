b4ba9520cee544cdabb57b9af81fa2776aeb5c2c
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-17 21:08:08 +0800

    forgot to fix again

--- src/options/base.ts
+++ src/options/base.ts
@@ -7,7 +7,7 @@
 import { App, PluginSettingTab, Setting } from "obsidian";
 import { timeFormat } from "../utils/values/timeFormat";
 import { generateFact } from "../functions/funFact";
-import { focusTimeModal } from "../modals/focusTimeModal";
+import { FocusTimeModal } from "../modals/focusTimeModal";
 import PerformiumPlugin from "../main";
 
 export interface PerformiumBaseSettings {

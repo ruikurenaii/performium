7309cf0fc07ad68d4fa2fb9789353417796aa681
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-17 18:49:10 +0800

    Update focusTimeModal.ts

--- src/modals/focusTimeModal.ts
+++ src/modals/focusTimeModal.ts
@@ -1,4 +1,6 @@
 import { App, Modal } from "obsidian";
+import { PerformiumBaseSettings, PerformiumSettingsTab, DEFAULT_SETTINGS } from "./options/base";
+import { getUptime } from "../functions/getUptime";
 
 export class focusTimeModal extends Modal {
   totalFocusTime: number;
@@ -10,6 +12,18 @@ export class focusTimeModal extends Modal {
 
   onOpen() {
     const { contentEl } = this;
+		contentEl.empty();
+	  contentEl.addClass("window-class");
+
+	  const time: string = getUptime(this.settings.totalFocusTime);
+
+		this.setTitle("Total focus time");
+
+		// add a temporary css class (will be changed in a few more commits)
+		contentEl.createEl("p", {
+      text: time,
+	    cls: "window-value"
+	  });
   }
 
   onClose() {

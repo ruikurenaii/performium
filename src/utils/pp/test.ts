19b973328d915c258bb73f1adcb2b3187d5a6e56
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-17 21:04:28 +0800

    pass settings as an argument

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -23,7 +23,7 @@ export async function calculatePerformance(app: App): Promise<number> {
   
   let wordsPerFile = vaultStats.averageWordsPerFile;
 
-  const totalFocusTime = this.settings.totalFocusTime;
+  const totalFocusTime = this.settings.totalFocusTime ?? 0;
   
   // for star rating
   const wordsPerSentence = words / sentences;

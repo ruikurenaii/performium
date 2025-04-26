5f5bb2a9d5c4eb2f5d62610c223033023451146c
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-26 12:33:52 +0800

    Create getAllTags.ts

---
+++ src/functions/getAllTags.ts
@@ -0,0 +1,21 @@
+import { TFile, normalizePath } from 'obsidian';
+
+async function getAllTags(app: App): Promise<Set<string>> {
+	const allTags = new Set<string>();
+	const metadataCache = app.metadataCache;
+	const vault = app.vault;
+
+	const files = vault.getMarkdownFiles();
+
+	for (const file of files) {
+		const cache = metadataCache.getFileCache(file);
+		if (!cache) continue;
+
+		const tagsInFile = getAllTags(cache);
+		for (const tag of tagsInFile) {
+			allTags.add(tag);
+		}
+	}
+
+	return allTags;
+}

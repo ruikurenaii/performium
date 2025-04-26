de95c34435cca35bda24aa68de3c9372f0dafd88
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-04-26 10:38:04 +0800

    remove the test pp system rewrite and continue the current pp system

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -1,43 +1,88 @@
 /*
 
   test.ts: the test version of the osu! pp system integrated into obsidian.
-  this is where the pp system is being reworked before releasing as a separate pp system version, but it still may vary depending on the demands.
-
+  the previous test version didn't get to be released, and due to countless hours of working on the issue why it didn't work, i decided to move it somewhere else.
+  don't worry, it won't be included to the plugin itself, but unless other people want to help me test it once more and tell what the issue was.
+  the pp rewrite didn't seem to go well, so we'll just continue what we're gonna focus on.
+  
 */
 
 import { App } from "obsidian";
 import { calculateVaultStats } from "../../functions/vaultStats";
-import { PerformiumBaseSettings, PerformiumSettingsTab, DEFAULT_SETTINGS } from "../../options/base";
 
 // the function to calculate the pp values from the entire vault (confusion, my bad)
 export async function calculatePerformance(app: App): Promise<number> {
-	const vaultStats = await calculateVaultStats(app);
-  
-  // variables
-  let files = vaultStats.totalFiles;
-  let tags = vaultStats.totalTags;
-  let words = vaultStats.totalWords;
-  let sentences = vaultStats.totalSentences;
-  let characters = vaultStats.totalChars;
-  let paragraphs = vaultStats.totalParagraphs;
-  
-  let wordsPerFile = vaultStats.averageWordsPerFile;
+  const vaultStats = await calculateVaultStats(app);
 
-  const totalFocusTime = this.settings.totalFocusTime ?? 0;
-  
-  // for star rating
-  const wordsPerSentence = words / sentences;
-  const sentencesPerParagraph = sentences / paragraphs;
-  const wordComplexity = characters / words;
-  const patternDifficulty = vaultStats.longestParagraphLength / (vaultStats.averageParagraphLength + 1);
-  
-  const starRating: number = Math.min(15, (wordsPerSentence * sentencesPerParagraph * wordComplexity * patternDifficulty) ** 0.25);
+  // weights
+  let a = 1.7349285739;
+  let b = 1.6893741205;
+  let c = 1.6228493017;
+  let d = 1.3428593012;
+  let e = 1.8753012946;
+  let f = 1.2109483725;
+
+  // factors
+  let penaltyFactor = 9.129727134;
 	
-  // bonuses
-  const lengthBonus = vaultStats.longestParagraphLength / vaultStats.longestSentenceLength;
-  // const timeBonus: number = Math.sqrt(Math.sqrt(totalFocusTime / (Math.sqrt(totalFocusTime) / totalFocusTime)));
+  // note complexity values for the system
+  const fileValue = vaultStats.totalFiles * (1 + (vaultStats.totalFolders / 25));
+	
+  const sentenceComplexityValue = vaultStats.totalWords / vaultStats.totalSentences;
+  const sentenceDensityValue = vaultStats.totalSentences / vaultStats.totalWords;
+  const fileComplexityValue = sentenceComplexityValue * vaultStats.averageWordsPerFile;
+  const wordComplexityValue = vaultStats.totalChars / vaultStats.totalWords;
 
-  const performanceValue: number = files + wordsPerFile + tags + lengthBonus + (1 + (starRating * 0.1)) /* + (timeBonus / 9.8) */;
+  // bonuses and nerfs
+  let sentenceBonus = 0;
+
+  if (vaultStats.averageSentencesPerFile > vaultStats.averageSentencesPerParagraph) {
+    sentenceBonus = vaultStats.averageSentencesPerFile / vaultStats.averageSentencesPerParagraph;
+  } else if (vaultStats.averageSentencesPerFile < vaultStats.averageSentencesPerParagraph) {
+	  sentenceBonus = vaultStats.averageSentencesPerParagraph / vaultStats.averageSentencesPerFile;
+  }
+
+  const readabilityMultiplier = 100 - ((vaultStats.averageWordsPerSentence * (vaultStats.averageCharsPerSentence / vaultStats.averageWordsPerSentence)) / 10);
+  const readabilityBonus = (sentenceDensityValue / sentenceDensityValue) * (readabilityMultiplier / 10);
+	
+  const overallComplexityValue = a * sentenceComplexityValue + b * sentenceDensityValue + c * (vaultStats.totalWords / vaultStats.totalFiles) + d * wordComplexityValue + e * sentenceBonus + f * readabilityBonus;
+
+  const lengthBonus = vaultStats.longestParagraphLength / vaultStats.longestSentenceLength;
+  const sLengthBonus = vaultStats.longestSentenceLength / vaultStats.averageSentenceLength;
+  const paragraphBonus = vaultStats.longestParagraphLength / vaultStats.averageParagraphLength;
+
+  const totalLengthBonus = (sLengthBonus + paragraphBonus) / lengthBonus;
+
+  const coherenceBonus = vaultStats.averageWordsPerSentence > 0 ? vaultStats.averageSentencesPerParagraph / vaultStats.averageWordsPerSentence : 0;
+
+  const informativenessValue = vaultStats.totalWords * (vaultStats.totalChars / vaultStats.totalWords);
+
+  const readingLevel = (0.39 * vaultStats.averageWordsPerSentence + 11.8 * (vaultStats.averageCharsPerSentence / vaultStats.averageWordsPerSentence) - 15.59) / 5;
+
+  const readingBonus = readabilityBonus * readingLevel * readabilityMultiplier;
+
+  const alternativeReadabilityValue = 0.39 * vaultStats.averageWordsPerSentence + 11.8 * wordComplexityValue;
+  let shortWordsNerf = 0;
+
+	const totalFocusTime = this.settings.totalFocusTime ?? 0;
+
+	const timeBonus: number = Math.sqrt(Math.sqrt(totalFocusTime / (Math.sqrt(totalFocusTime) / totalFocusTime)));
+	let finalTimeBonus: number = 0;
+
+	if (timeBonus >= 241.97) {
+		finalTimeBonus = 241.97 + (timeBonus / 1000);
+	} else {
+		finalTimeBonys = timeBonus;
+	}
+
+  if (alternativeReadabilityValue > 25) {
+	  shortWordsNerf = alternativeReadabilityValue * (wordComplexityValue / 5);
+  } else if (alternativeReadabilityValue < 25) {
+	  shortWordsNerf = alternativeReadabilityValue - penaltyFactor * (wordComplexityValue / 5);
+  }
+														
+  // gotta prevent inflation :)))))
+  const performanceValue: number = (fileValue + overallComplexityValue + totalLengthBonus + finalTimeBonus + coherenceBonus + (informativenessValue ** 0.3825) + (readingBonus ** 0.5) + shortWordsNerf) / 1.7724538509;
 
   return performanceValue;
-}
+}  

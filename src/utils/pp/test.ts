c901fe2e57e766e339595c009b08cd55cd423791
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-05-09 13:13:51 +0000

    pp rebalance + update plugin version

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -188,7 +188,7 @@ export async function calculatePerformance(plugin: PerformiumPlugin): Promise<nu
     combinedValue *= 1 + ((starRating - 5) / 8.92);
   }
 
-  const performanceValue: number = ((angleBonus + starRatingBonus) / 2.05) + (combinedValue * (starRating / 2.3)) + (roughnessPenalty * (3.1415926535 / 0.875)) + (factorBonus / (2.7182818284 * 1.05)) + (overallPenalty / -1.1) + (timeBonus / 9.8) + bonusValue + lengthBonus + (burstScore / 3.57);
+  const performanceValue: number = ((angleBonus + starRatingBonus) / 2.05) + (combinedValue * (starRating / 2.3)) + (roughnessPenalty * (3.1415926535 / 0.875)) + (factorBonus / (2.7182818284 * 1.05)) + (overallPenalty / -1.1) + (timeBonus / 9.8) + bonusValue + Math.pow(lengthBonus, 0.41) + (burstScore / 1.026);
   
   return performanceValue;
 }

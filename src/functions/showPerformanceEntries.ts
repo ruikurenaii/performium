3eef00acfc2e4c6652e170d80a57720ed02f2b32
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-05-27 22:46:27 +0800

    Update showPerformanceEntries.ts

--- src/functions/showPerformanceEntries.ts
+++ src/functions/showPerformanceEntries.ts
@@ -1,6 +1,6 @@
 import { App } from "obsidian";
-import { HighestPerformanceModal } from "./modals/highestPerformanceModal";
-import { getTopPerformanceEntries } from "./functions/getTopPerformanceEntries";
+import { HighestPerformanceModal } from "../modals/highestPerformanceModal";
+import { getTopPerformanceEntries } from "./getTopPerformanceEntries";
 
 export async function showAllPerformanceEntries(app: App) {
   const entries = await getAllPerformanceEntries(app);

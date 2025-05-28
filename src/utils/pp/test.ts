d7d033f80a883df1d3dcf88307a299456a8f5d18
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-05-28 14:08:08 +0800

    rework execution count bonus pp

--- src/utils/pp/test.ts
+++ src/utils/pp/test.ts
@@ -207,7 +207,7 @@ export async function calculatePerformance(plugin: PerformiumPlugin): Promise<nu
   const charAngleBonus: number = (totalChars / 968.75) * (1 + (0.45 * (angleValue / 360)));
 	
   // add bonus pp based on how many times the performance points calculation has been executed.
-  const executionBonus: number = (417 - (1 / 3)) * (1 - Math.pow(0.9975, totalExecutionCount));
+  const executionBonus: number = ((417 - (1 / 3)) / 2) * (1 - Math.pow(0.9975, totalExecutionCount));
 
   // add a file count pp bonus, which should stop at around 27,608 files
   const fileCountBonus: number = (417 - (1 / 3)) * (1 - Math.pow(0.9996, totalFiles));

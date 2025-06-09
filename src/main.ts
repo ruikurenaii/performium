56455ba25503147fdca607f326e9e081a462c315
Louie Soulen Kurenai <168345503+ruikurenaii@users.noreply.github.com>
2025-06-09 16:24:45 +0800

    integrate the new pp system

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { calculatePerformance as calculatePerformance041325 } from "./utils/pp/0
 import { calculatePerformance as calculatePerformance042925 } from "./utils/pp/042925";
 import { calculatePerformance as calculatePerformance050725 } from "./utils/pp/050725";
 import { calculatePerformance as calculatePerformance051425 } from "./utils/pp/051425";
+import { calculatePerformance as calculatePerformance060925 } from "./utils/pp/0060925";
 import { calculatePerformance as calculatePerformanceTest } from "./utils/pp/test";
 
 export default class PerformiumPlugin extends Plugin {
@@ -111,7 +112,9 @@ export default class PerformiumPlugin extends Plugin {
   async calculatePerformance(): Promise < number > {
     if (this.settings.ppSystem === "test") {
       return calculatePerformanceTest(this);
-    } else if (this.settings.ppSystem === "051425") {
+    } else if (this.settings.ppSystem === "060925") {
+      return calculatePerformance060925(this);
+	} else if (this.settings.ppSystem === "051425") {
       return calculatePerformance051425(this);
 	  } else if (this.settings.ppSystem === "050725") {
       return calculatePerformance050725(this);
@@ -127,7 +130,9 @@ export default class PerformiumPlugin extends Plugin {
   async calculateSecondaryPerformance(): Promise < number > {
     if (this.settings.secondaryPpSystem === "test") {
       return calculatePerformanceTest(this);
-    } else if (this.settings.secondaryPpSystem === "051425") {
+    } else if (this.settings.ppSystem === "060925") {
+      return calculatePerformance060925(this);
+	} else if (this.settings.secondaryPpSystem === "051425") {
       return calculatePerformance051425(this);
 	  } else if (this.settings.secondaryPpSystem === "050725") {
       return calculatePerformance050725(this);

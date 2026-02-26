95e1243b4e07b24d151feed9212e719502d6d520
Louie Sebastian Kurenai <paytousebloxy1774@gmail.com>
2026-02-26 21:00:58 +0800

    add new function

--- src/utils/values/newerEvaluators/newVaultStats.ts
+++ src/utils/values/newerEvaluators/newVaultStats.ts
@@ -1,4 +1,4 @@
-import { App } from 'obsidian';
+import { App, TFile } from 'obsidian';
 
 export async function getTotalWordCount(app: App): Promise<number> {
     const files = app.vault.getMarkdownFiles();
@@ -18,4 +18,23 @@ function countWords(content: string): number {
     content = content.replace(/[[\]`#*_~>]/g, ' ');
     
     return content.split(/\s+/).filter(word => word.length > 0).length;
+}
+
+export async function getTotalSentences(app: App): Promise<number> {
+  const files = app.vault.getMarkdownFiles();
+  let totalSentences = 0;
+  
+  for (const file of files) {
+    if (file instanceof TFile) {
+      const content = await app.vault.read(file)
+
+      const sentences = content.match(/[^.!?]+[.!?]+/g);
+  
+      if (sentences) {
+        totalSentences += sentences.length;
+      }
+    }
+  }
+  
+  return totalSentences;
 }
